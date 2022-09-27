eac9c87736d73bdc771bf16e4f1201b2558b1d2d
Keegan77 <keegansb77@gmail.com>
2022-09-27 18:41:35 -0400

    Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody rb;
+    private GameObject player;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
