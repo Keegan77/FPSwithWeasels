685845836ed52c8ad58b9f9c2fd2802fe9ff4d5e
Keegan77 <keegansb77@gmail.com>
2022-09-27 23:56:07 -0400

    added collision ramps for smoother colls
    Co-Authored-By: Ronan-Cords <71858463+Ronan-Cords@users.noreply.github.com>

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class SpawnEnemies : MonoBehaviour
 {
     public GameObject enemy;
+    private Transform tf;
     private int timerStart = 3;
     private float timer = 3;
     // Start is called before the first frame update
     void Start()
     {
-
+        tf = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class SpawnEnemies : MonoBehaviour
         timer = timer - Time.deltaTime;
         if (timer < 0)
         {
-            Instantiate(enemy, gameObject.transform);
+            Instantiate(enemy, tf);
             timer = timerStart;
         }
         Debug.Log(timer);
